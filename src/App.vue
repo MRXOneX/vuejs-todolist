98b6d11292a107ab114d6ad39bf98a5906b587d0
Misha Poleshchenkov <74454444+MRXOneX@users.noreply.github.com>
2022-02-02 23:33:40 +0200

    added postitem, postlist,postfrom, ui-components,

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Hello world
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <form @submit.prevent>
+        <h4>Create post</h4>
+        <my-input
+            v-focus
+            v-model="post.title"
+            type="text"
+            placeholder="Title"
+        />
+        <my-input 
+            v-model="post.body"
+            type="text"
+            placeholder="Description"
+        />
+        <my-button
+            style="align-self: flex-end; margin-top: 15px;"
+            @click="createPost"
+        >
+            Create
+        </my-button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                id: '',
+                title: '',
+                body: ''
+            }
+        }
+    },
+    methods: {
+        createPost() {
+            this.$emit('create', this.post)
+            this.post = {
+                id: id = Date.now(),
+                title: '',
+                body: ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
